8c90fa358ceef0c7c12757fd1d47a20f4888cfc6
Sergo-Kaz13 <13fenrill13666@gmail.com>
2024-09-29 17:06:43 +0200

    finish

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
